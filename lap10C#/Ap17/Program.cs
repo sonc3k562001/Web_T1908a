9b0eaeba2428138746c5036d12eb97f6c5341262
Nguyễn Giang Sơn <son10c3k56@gmail.com>
2020-05-28 23:39:32 +0700

    C#

---
+++ lap10C#/Ap17/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApp
+{
+    public delegate string MyDel(string str);
+
+    class EventProgram
+    {
+        event MyDel MyEvent;
+        public EventProgram()
+        {
+            this.MyEvent += new MyDel(this.WelComeUser);
+        }
+        public string WelComeUser(string username)
+        {
+            return "Welcome" + username;
+        }
+
+        static void Main(string[] args)
+        {
+            EventProgram obj1 = new EventProgram();
+            string result = obj1.MyEvent("Tutorials Point");
+            Console.WriteLine(result);
+        }
+
+    }
+}
