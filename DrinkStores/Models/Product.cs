a0c0bce90556fedf6228d7b11b0d5780cc9141e9
sonc3k562001 <54662662+sonc3k562001@users.noreply.github.com>
2020-11-13 21:02:53 +0700

    ok

---
+++ DrinkStores/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DrinkStores.Models
+{
+    public class Product
+    {
+        public long ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Img { get; set; }
+        public decimal Price { get; set; }
+        [Column(TypeName ="decimal(8,2)")]
+        public double Discount { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ DrinkStores/Models/SeedData.cs
@@ -0,0 +1,80 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrinkStores.Models
+{
+    public static class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            StoreDbContext context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<StoreDbContext>();
+
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+            if (!context.Products.Any())
+            {
+                context.Products.AddRange(
+                    new Product
+                    {
+                        Name = "Water",
+                        Description= "this is a category use for drink",
+                        Img = "...MilkTea.png",
+                        Price = 123,
+                        Discount = 1,
+                        Status = "stocking"
+                    },
+                    new Product
+                    {
+                        Name = "Mikl Tea",
+                        Description = "this is a category use for drink",
+                        Img = "...MilkTea.png",
+                        Price = 123,
+                        Discount = 1,
+                        Status = "stocking"
+                    },
+                    new Product
+                    {
+                        Name = "Mikl Tea",
+                        Description = "this is a category use for drink",
+                        Img = "...MilkTea.png",
+                        Price = 123,
+                        Discount = 1,
+                        Status = "stocking"
+                    },
+                    new Product
+                    {
+                        Name = "Mikl Tea",
+                        Description = "this is a category use for drink",
+                        Img = "...MilkTea.png",
+                        Price = 123,
+                        Discount = 2,
+                        Status = "stocking"
+                    },
+                    new Product
+                    {
+                        Name = "Mikl Tea",
+                        Description = "this is a category use for drink",
+                        Img = "...MilkTea.png",
+                        Price = 123,
+                        Discount = 2,
+                        Status = "stocking"
+                    },
+                    new Product
+                    {
+                        Name = "Mikl Tea",
+                        Description = "this is a category use for drink",
+                        Img = "...MilkTea.png",
+                        Price = 123,
+                        Discount = 2,
+                        Status = "stocking"
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
