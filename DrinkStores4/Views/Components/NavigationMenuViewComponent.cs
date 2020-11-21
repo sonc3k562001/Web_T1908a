cc840e068a84ce4cd23be898f480b4776833946e
sonc3k562001 <54662662+sonc3k562001@users.noreply.github.com>
2020-11-22 03:58:14 +0700

    ok

---
+++ DrinkStores4/Views/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using DrinkStores.Models;
+
+
+namespace DrinkStores.Views.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IStoreRepository repository;
+        public NavigationMenuViewComponent(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    //    public string Invoke()
+    //    {
+    //        return "Son Dep Trai";
+    //    }
+    }
+}
---
+++ DrinkStores4/Views/Components/NavigationMenuViewComponents.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DrinkStores.Views.Components
+{
+    public class NavigationMenuViewComponents : ViewComponent //tạo view components để có thể sử dụng lại trong các trang web
+    {
+        public string Invoke()
+        {
+            return "Hello from the NGuyen Giang SOn Page";
+        }
+    }
+}
