2071601eee5bedf122d062deff138b4349bff84b
sonc3k562001 <54662662+sonc3k562001@users.noreply.github.com>
2020-12-26 04:15:32 +0700

    bản thử nghiệm

---
+++ WebApplication1/Controllers/ForgotPassword.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ForgotPassword : Controller
+    {
+        [Route("Indentity/Account/ForgotPassword")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
